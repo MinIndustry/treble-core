e38a7f6100e159a1b809f11c6e5937b40804a579
Minigrim0 <fgrimau@proton.me>
2026-06-10 15:12:21 +0100

    [Fix] test source map entries as well

--- tests/unit/app/audio_graph.rs
+++ tests/unit/app/audio_graph.rs
@@ -1,4 +1,4 @@
-use treble::{app::audio_graph::AudioGraph, instruments::prelude::{Kick, Snare}, score::compiled_score};
+use treble::{app::audio_graph::AudioGraph, instruments::prelude::{Kick, Snare}};
 
 #[test]
 pub fn test_audio_graph_compilation_isnt_destructive() {
@@ -8,10 +8,12 @@ pub fn test_audio_graph_compilation_isnt_destructive() {
     assert!(maybe_compiled_graph.is_ok(), "Audio graph compilation should be working fine");
     let compiled_graph = maybe_compiled_graph.unwrap();
     assert_eq!(compiled_graph.sources_len(), 1, "There should be a single source in the compiled graph with a single instrument");
+    assert_eq!(audio_graph.source_map.iter().len(), 1, "There should be a single entry in the source map of the audio graph");
 
     audio_graph.add_instrument(Box::from(Snare::new()));
     let maybe_compiled_graph = audio_graph.compile(44100.0);
     assert!(maybe_compiled_graph.is_ok());
     let compiled_graph = maybe_compiled_graph.unwrap();
-    assert_eq!(compiled_graph.sources_len(), 2, "There should be two sources for a graph with two instruments.")
+    assert_eq!(compiled_graph.sources_len(), 2, "There should be two sources for a graph with two instruments.");
+    assert_eq!(audio_graph.source_map.iter().len(), 2, "There should be two distinct entries in the source map of the audio graph");
 }
